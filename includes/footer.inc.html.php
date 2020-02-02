aefa9616d4a486feb87efb4a64964d7604ce7b93
DawnShreves <60557581+DawnShreves@users.noreply.github.com>
2020-02-02 10:07:11 -0800

    ace in the hole website files

---
+++ includes/footer.inc.html.php
@@ -0,0 +1,8 @@
+<footer>
+        <p>Follow us in Facebook and twitter!</p>
+        <a class="social" href="https://www.facebook.com/cas222cascade/community/" target="_blank"><img src="../images/fblogo.png"></a>
+        <a class="social" href="https://twitter.com/pcccas222?lang=en" target="_blank"><img src="../images/tlogo.png"></a>
+        
+        <small>Copyright©2020 By Dawn Shreves</small><br>
+        <small>Ace in the Hole Multisport Event</small><br>
+    </footer
